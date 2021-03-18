0cd0d04731bc88a98339fbedfd9ea0513d75e50a
克格勃佳佳 <jiasansui1573@gmail.com>
2021-03-18 16:15:20 +0800

    Solution1_1 01

--- src/Chapter01/Solution1_1.java
+++ src/Chapter01/Solution1_1.java
@@ -1,4 +1,15 @@
 package Chapter01;
 
 public class Solution1_1 {
+    public static void main(String[] args) throws InterruptedException {
+       solution01();
+    }
+    private static void solution01() throws InterruptedException {
+       while(true){
+           for(int i=0;i<9600000;++i){
+
+           }
+           Thread.sleep(1);
+       }
+    }
 }
